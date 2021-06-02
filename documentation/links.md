ed19367454931159318d8199562009ff20ce6b76
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2021-06-02 08:17:36 +0200

    [main] rest discovery fixed

--- documentation/links.md
+++ documentation/links.md
@@ -1,1 +1,5 @@
+# Pages about multiple modules projects 
 https://www.baeldung.com/spring-boot-multiple-modules
+https://spring.io/guides/gs/multi-module/
+https://developer.okta.com/blog/2020/07/27/spring-boot-using-java-modules
+https://dzone.com/articles/ddd-spring-boot-multi-module-maven-project
--- orchestration/orchestration.main.completeservice/src/main/java/com/devonfw/training/hexagonal/orchestration/main/completeservice/Application.java
+++ orchestration/orchestration.main.completeservice/src/main/java/com/devonfw/training/hexagonal/orchestration/main/completeservice/Application.java
@@ -3,7 +3,7 @@ package com.devonfw.training.hexagonal.orchestration.main.completeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.devonfw.training.hexagonal")
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
